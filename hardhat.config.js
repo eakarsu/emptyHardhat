861a1c4e20f7ac7bd9792ded3882a48c5722ddc7
Erol Akarsu <eakarsu@gmail.com>
2022-01-03 08:45:46 -0500

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,67 @@
+require("dotenv").config();
+
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+
+const mnemonic="test test test test test test test test test test test junk";
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.6.12",
+  networks: {
+    ropsten: {
+      url: process.env.ROPSTEN_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+
+    hardhat: {
+      accounts: {
+        accountsBalance: "100000000000000000000000000000000000000",
+        count: 10,
+        mnemonic,
+      },
+      //chainId: 31337,
+      chainId: 1,
+      forking: {
+        //blockNumber: 13619920, // Nov 15, 2021
+        url: "https://eth-mainnet.alchemyapi.io/v2/4gZBZgXw-GGmzJF3R6FTJvXKNK_XDTJn",
+      },
+    },
+
+    localhost: {
+      accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80","0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d","0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a"],
+      chainId: 1,
+      //chainId: 31337,
+      loggingEnabled: true,
+
+      url: "http://localhost:8545"
+    },
+
+
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
