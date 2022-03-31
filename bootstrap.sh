f2aa59b4a1c85e79c6a2605d6f80369b379aad4f
Erol Akarsu <eakarsu@gmail.com>
2022-03-31 16:27:46 -0400

    keep container running with tail

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,9 +10,9 @@ npm run aave:fork:main
 #unpause lending pool
 npx hardhat unpause  --localuseraddress 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266 --hardhathost http://localhost:8545
 
-
 cd /home/Research/enzyme
 npx hardhat --network localhost deploy
 #fix yearn vault issue
 npx hardhat yearnvault --yearnvaultgovernance 0xFEB4acf3df3cDEA7399794D0869ef76A6EfAff52  --yvault 0x19d3364a399d251e894ac732651be8b0e4e85001 --hardhathost http://localhost:8545
 
+tail -F -n0 /etc/hosts
