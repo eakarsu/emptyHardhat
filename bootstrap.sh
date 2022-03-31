849a6709c4cd4c5497b48a1229fcad2b11d525e1
Erol Akarsu <eakarsu@gmail.com>
2022-03-31 15:13:30 -0400

    run hardtask to fix lending pool unpause and yearn vault issue

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,12 @@ sleep 10s
 
 cd /home/Research/aave
 npm run aave:fork:main
+#unpause lending pool
+npx hardhat unpause  --localuseraddress 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266 --hardhathost http://localhost:8545
+
 
 cd /home/Research/enzyme
 npx hardhat --network localhost deploy
+#fix yearn vault issue
+npx hardhat yearnvault --yearnvaultgovernance 0xFEB4acf3df3cDEA7399794D0869ef76A6EfAff52  --yvault 0x19d3364a399d251e894ac732651be8b0e4e85001 --hardhathost http://localhost:8545
+
